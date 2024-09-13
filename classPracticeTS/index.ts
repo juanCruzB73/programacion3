848bc0043b01c071bfd7e945273f0705770a5c45
Juan Cruz <juancruzberrios73@gmail.com>
2024-09-12 21:21:23 -0300

    ts

---
+++ classPracticeTS/index.ts
@@ -0,0 +1,86 @@
+/*
+ Ejercicio 1: Clases y Métodos
+Descripción: Crea una clase Coche que tenga propiedades como marca, 
+modelo y velocidad. Implementa métodos para acelerar, frenar y mostrar el estado del coche.
+
+Requerimientos:
+Define una clase Coche con un constructor.
+Implementa métodos acelerar y frenar que modifiquen la velocidad.
+Crea un método para mostrar el estado actual del coche.
+ */
+
+class Car {
+	brand: string;
+
+	model: string;
+
+	speed: number;
+
+	state: boolean;
+	
+	  constructor(brand: string, model: string, speed: number, state: boolean) {
+   		 this.brand = brand;
+    		 this.model = model;
+		 this.speed=speed;
+		 this.state=state;
+  	  }
+
+	speedUp(speed:number):number{
+		return speed+1;
+	};
+
+
+	breakCar(speed: number, state:boolean): void{
+		if(state == true && speed>0){
+			speed=0;
+			console.log("se detuvo el auto");
+		}else{
+			console.log("el auto no esta encendido o no tiene velocidad");
+		}
+	};
+
+	turnOnAndOf(state:boolean): boolean {
+		if(state==true){         
+			console.log("se apago el auto")
+		       	return state=false;
+                }else{
+			
+                        console.log("se encendio el auto");
+			return state=true;
+                
+		}
+	
+	};
+	showState(state:boolean): void{
+		if(state==true){ 	
+			console.log("el auto esta encendido");
+		}else{
+			console.log("el auto esta apagado");
+		}
+	};
+}
+
+const car1 = new Car("toyota","AE86 trueno",160,true);
+console.log("el marca es "+car1.brand);
+console.log("el modelo es "+car1.model);
+console.log("la velocidad es " +car1.speed);
+car1.breakCar(car1.speed,car1.state)
+console.log("la velocidad es de "+car1.speed);
+console.log(car1.state);
+car1.turnOnAndOf(car1.state);
+console.log(car1.state);
+
+//ejercicio 2;
+/*
+Ejercicio 2: Peticiones Asíncronas con Fetch
+Descripción: Realiza una petición fetch a una API pública (por ejemplo, jsonplaceholder)
+ para obtener una lista de tareas y filtra aquellas que están completadas.
+Requerimientos:
+Usa fetch para realizar la petición.
+Define una interfaz para los datos recibidos.
+Filtra las tareas completadas y muéstralas en la consola.
+
+link de api: https://jsonplaceholder.typicode.com/todos
+*/
+
+
