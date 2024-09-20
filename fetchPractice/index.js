9ad216180834f43853dcde92f95eb5cbb700828e
Juan Cruz <juancruzberrios73@gmail.com>
2024-09-20 17:52:53 -0300

    cambios en trabajos de fetch

--- fetchPractice/index.js
+++ fetchPractice/index.js
@@ -1,25 +1,31 @@
-//EJERCICIO1
 const api='https://randomuser.me/api';
-async function getText(api){
-    let x= await fetch(api)
-    let y=await x.json();
-    let person=y.results[0];
 
-    let name=person.name.title+" "+person.name.first+" "+person.name.last;
-    let gender=person.gender;
-    let adress=person.location.city+" "+person.location.state+" "+person.location.country;
-    let picture=person.picture.large;
-    function writePerson(){
-        personContainer.innerHTML+=`<p>${name}</p>`
-        personContainer.innerHTML+=`<p>${gender}</p>`
-        personContainer.innerHTML+=`<p>${adress}</p>`
-        personContainer.innerHTML+=`<img src="${picture}"/>`
-    }
-    writePerson();
+const getDatos=(api)=>{
+    return new Promise((resolve,reject)=>{
+        if(api){
+            resolve(fetch(api)) 
+        }else{
+            reject("error")
+        }          
+    })
 }
-let personContainer=document.getElementById("personContainer")
-getText(api);
-
+getDatos(api).then((result)=>{
+        result.json().then(jsonData=>{
+            let person=jsonData.results[0];
+            let name=person.name.title+" "+person.name.first+" "+person.name.last;
+            let gender=person.gender;
+            let adress=person.location.city+" "+person.location.state+" "+person.location.country;
+            let picture=person.picture.large;
+            let personContainer=document.querySelector("#personContainer")
+            function writePerson(){
+                personContainer.innerHTML+=`<p>${name}</p>`
+                personContainer.innerHTML+=`<p>${gender}</p>`
+                personContainer.innerHTML+=`<p>${adress}</p>`
+                personContainer.innerHTML+=`<img src="${picture}"/>`
+            }
+            writePerson()         
+        }).catch(error=>"error pasando al json o desplegando", error)
+    }).catch(error=>"error en la primera promesa")
 //EJERCICIO 2
 const api1='https://jsonplaceholder.typicode.com/posts';
 async function getTitles(api){
@@ -29,7 +35,6 @@ async function getTitles(api){
     for(let i=0;i<10;i++){
         titles.push(y[i].title)
     }
-    console.log(titles)
     function writeTitles(){
         let counter =1;
         titles.forEach(e=>{
@@ -41,3 +46,19 @@ async function getTitles(api){
 }
 let titleContainer=document.getElementById("personContainer1")
 getTitles(api1);
+//EJERCICIO 3
+/*
+**Mostrar la temperatura actual**
+   - **API Endpoint**: https://api.openweathermap.org/data/2.5/weather
+   - **Documentación**: https://openweathermap.org/current
+   - **Descripción**: OpenWeatherMap ofrece datos meteorológicos actuales, previsiones,
+    e históricos mediante su API. En este ejercicio, se consulta el endpoint "/weather"
+     para obtener la temperatura y la descripción del clima actual de la ciudad de Mendoza, 
+     Argentina. Los resultados se muestran en el contenedor HTML con el id="weatherInfo"
+   -**recursos** 
+            --apikey : c61e0400e304a834d4159274cfc06146
+            
+    -**ayudas**
+        -parametros a utilizar =  units, lang, 
+        -ver como pasar el parametro de pais y region link:https://openweathermap.org/current#name
+*/
